4cdf539727db9dad66272d23ed6a89bd13bb186f
JERF21 <eduardo21reyes@gmail.com>
2024-08-21 20:16:34 -0400

    Cliente 2

--- assest/js/cliente.js
+++ assest/js/cliente.js
@@ -1,167 +1,137 @@
 function MNuevoCliente(){
-    $("#modal-default").modal("show");
-   
-    var obj="";
-    $.ajax({
-   
-       type:"POST",
-       url:"vista/cliente/FNuevoCliente.php",
-       data: obj,
-       success: function(data) {
-           $("#content-default").html(data);
-       }
-    })
-   }
+    $("#modal-xl").modal("show");
 
-function regCliente(){
- 
+    var obj=""
+    $.ajax({
+        type:"POST",
+        url:"vista/cliente/FNuevoCliente.php",
+        data:obj,
+        success:function(data){
+            $("#content-x1").html(data)
+        }
+    })
+}
+
+function regCliente() {
     var formData=new FormData($("#FRegCliente")[0])
 
-
+    //if(formData.get("password")==formData.get("vrPassword")){
         $.ajax({
-       
-           type:"POST",
-           url:"controlador/clienteControlador.php?ctrRegCliente",
-           data: formData,
-           cache:false,
-           contentType:false,
-           processData:false,
-           success: function(data) {
-               
-            if(data="ok"){
+            type:"POST",
+            url:"controlador/clienteControlador.php?ctrRegCliente",
+            data:formData,
+            cache:false,
+            contentType:false,
+            processData:false,
+            success:function(data){
 
-                Swal.fire({
-                    icon: 'success',
-                    title: "Registro Exitoso",
-                    showConfirmButton: false,
-                    timer: 1000
-                    
-                });
-
-                setTimeout(function(){
-                    location.reload()
-                },1200)
+                // console.log(data)
+                
+                if(data="ok"){
+                    Swal.fire({
+                        icon:"success",
+                        showConfirmButton:false,
+                        title:"El cliente a sido registrado",
+                        timer:1000
+                    })
+                    setTimeout(function() {
+                        location.reload()
+                    },1200)
+                }else{
+                    Swal.fire({
+                        title:"ERROR!",
+                        icon:"error",
+                        showConfirmButton:false,
+                        timer:1000
+                    })
+                }
 
             }
-            else{
-                Swal.fire({
-                    title: "Error",
-                    icon: 'error',
-                    showConfirmButton: false,
-                    timer: 1000
-                    
-                });
-            }
-
-           }
-        })
-
-    
-
+        })
+    //}
 }
 
-function MEditCliente(id){
+function MEditCliente(id) {
+    $("#modal-warning").modal("show");
 
-    $("#modal-default").modal("show");
-   
-    var obj="";
+    var obj=""
     $.ajax({
-   
-       type:"POST",
-       url:"vista/cliente/FEditCliente.php?id="+id,
-       data: obj,
-       success: function(data) {
-           $("#content-default").html(data);
-       }
-    })
-
-
-
-    
-} //final
-function editCliente(){
-
-    var formData=new FormData($("#FEditCliente")[0])
-    if(formData.get("password")==formData.get("vrPassword")){
-
-        $.ajax({
-       
-           type:"POST",
-           url:"controlador/clienteControlador.php?ctrEditCliente",
-           data: formData,
-           cache:false,
-           contentType:false,
-           processData:false,
-           success: function(data) {
-
-            if(data="ok"){
-
-                Swal.fire({
-                    icon: 'success',
-                    showConfirmButton: false,
-                    title: "Cliente Actualizado",
-                    timer: 1000
-                    
-                });
-
-                setTimeout(function(){
-                    location.reload()
-                },1200)
-
-            }
-            else{
-                Swal.fire({
-                    title: "Error",
-                    icon: 'error',
-                    showConfirmButton: false,
-                    timer: 1000
-                    
-                });
-            }
-
-           }
-        })
-
-    }
-
-
+        type:"POST",
+        url:"vista/cliente/FEditCliente.php?id="+id,
+        data:obj,
+        success:function(data){
+            $("#content-warning").html(data)
+        }
+    })
 }
 
+function editCliente() {
+    var formData=new FormData($("#FEditCliente")[0])
+
+        $.ajax({
+            type:"POST",
+            url:"controlador/clienteControlador.php?ctrEditCliente",
+            data:formData,
+            cache:false,
+            contentType:false,
+            processData:false,
+            success:function(data){
+
+                console.log(data)
+                
+                if(data="ok"){
+                    Swal.fire({
+                        icon:"success",
+                        showConfirmButton:false,
+                        title:"El cliente ha sido ACTUALIZADO",
+                        timer:1000
+                    })
+                    setTimeout(function() {
+                        location.reload()
+                    },1200)
+                }else{
+                    Swal.fire({
+                        title:"ERROR!",
+                        icon:"error",
+                        showConfirmButton:false,
+                        timer:1000
+                    })
+                }
+
+            }
+        })
+}
 
 function MEliCliente(id){
-
     var obj={
         id:id
     }
-
-Swal.fire({
-    title:"¿Estás seguro de eliminar este cliente?",
-    showDenyButton:true,
-    showCancelButton:false,
-    confirmButtonText:'Confirmar',
-    denyButtonText:'Cancelar'
-}).then((result)=>{
-    if(result.isConfirmed){
-        $.ajax({
-            type:"POST",
-            url:"controlador/clienteControlador.php?ctrEliCliente",
-            data:obj,
-            success: function(data) {
-              if(data=="ok"){
-                location.reload()
-              }
-              else{
-                Swal.fire({
-                    icon: 'error',
-                    showConfirmButton: false,
-                    title: 'Error',
-                    text:'El cliente no puede ser eliminado',
-                    timer: 1000
-                    
-                });
-              }
-            }
-        })
-    }
-})
+    Swal.fire({
+        title:"¿Estas seguro?",
+        showDenyButton:true,
+        ShowCancelButton:false,
+        confirmButtonText:'Confirmar',
+        denyButtonText:'Cancelar'
+    }).then((result)=>{
+        if(result.isConfirmed){
+            $.ajax({
+                type:"POST",
+                url:"controlador/clienteControlador.php?ctrEliCliente",
+                data:obj,
+                success:function(data){
+                    if(data=="ok"){
+                        location.reload();
+                    }else{
+                        Swal.fire({
+                            icon:"error",
+                            showConfirmButton:false,
+                            title:'ERROR',
+                                text:'No pudimos eliminarlo',
+                            timer:1000
+                        })
+                    }
+                }
+            })
+        }
+    })
 }
